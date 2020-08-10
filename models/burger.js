48e00e7170d5288645f1c07a84e2dce38798215d
Erin Fisher <flybirdie85@gmail.com>
2020-08-10 12:41:51 -0700

    added content to index and update function to burger.js

--- models/burger.js
+++ models/burger.js
@@ -7,6 +7,11 @@ let burger = {
     });
   },
   // The variables cols and values are arrays
+  create: function (cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
   update: function (objColVals, condition, cb) {
     orm.update("burgers", objColVals, condition, function (res) {
       cb(res);
